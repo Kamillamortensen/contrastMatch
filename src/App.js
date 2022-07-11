18bda6321da74a1e874d71e442e7f7510f13841b
Kamilla Mortensen <kamilla@nr.no>
2022-07-11 11:36:23 +0200

    react i18next for språk

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import "./App.css";
 import React, { Suspense } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; //tried HashRouter not BrowserRouter
 import Home from "./pages/home/home";
 import About from "./pages/about/About";
+import { useTranslation } from 'react-i18next';
 
 const App = () => {
+  const { t } = useTranslation();
   return (
     <Suspense>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/contrastMatch" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </Router>
