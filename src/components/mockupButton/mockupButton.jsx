f46292a5bfd9279c22f6c1b2cc73a8f0f24fc530
Kamilla Mortensen <kamilla@nr.no>
2022-08-05 15:03:43 +0200

    color picker logic

--- src/components/mockupButton/mockupButton.jsx
+++ src/components/mockupButton/mockupButton.jsx
@@ -9,25 +9,6 @@ const MockupButton = ({ colorList }) => {
   const [colorMain, setColorMain] = useState(colorList.length >= 2 ? colorList[0] : "#ffffff"); 
   const [colorSecondary, setColorSecondary] = useState(colorList.length >= 2 ? colorList[1] : "#000000");
 
-  useEffect(() => {
-    if(colorList.length >= 2){
-      if(colorList.length == 2) setColorMain(colorList[0]); setColorSecondary(colorList[1]);
-      /*if(!colorList.includes(colorMain)) colorList[0] == colorSecondary ? setColorMain(colorList[1]) : setColorMain(colorList[0]);
-      if(!colorList.includes(colorSecondary)) colorList[0] == colorMain ? setColorSecondary(colorList[1]) : setColorSecondary(colorList[0]);*/
-    }
-    else {
-      if(colorList.length == 1){
-        setColorMain(colorList[0]);
-        setColorSecondary(colorList[0]);
-        return;
-      }
-      setColorMain("#ffffff");
-      setColorSecondary("#ffffff");
-      return;
-    }
-  }, [colorList]);
- 
-  console.log(colorList.length)
   return (
     <div className="mockupElement">
     <div className="mockup-color-bar">
